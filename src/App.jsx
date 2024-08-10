6f9a836e3394350376f2f91ce6ce65d62e94014a
vphucthinh <150200356+vphucthinh@users.noreply.github.com>
2024-08-10 19:37:33 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,14 @@ import Verify from './pages/Verify/Verify'
 import MyOrders from './pages/MyOrders/MyOrders'
 import Chatting from './pages/Chatting/Chatting'
 import Profile from './pages/Profile/Profile'
+const ZENDESK_KEY = "cc894698-9e45-40b1-89da-fe036719e123";
+import Zendesk, { ZendeskAPI } from "./ZendexConfig";
 
 const App = () => {
+
+  const handleLoaded = () => {
+    ZendeskAPI("messenger", "open");
+  };
 
 
   const [showLogin,setShowLogin] = useState(false)
@@ -31,6 +37,7 @@ const App = () => {
          <Route path='/chatting' element={<Chatting/>} />
          <Route path='/profile' elemnet={<Profile/>} />
       </Routes>
+      <Zendesk defer zendeskKey={ZENDESK_KEY} onLoaded={handleLoaded} />
     </div>
     <Footer/>
     </>
